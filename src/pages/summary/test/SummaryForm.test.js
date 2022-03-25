58f8ab70b46a3f21ccb7d649d366d065912b458f
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2022-03-24 21:55:15 -0300

    feat: add test to popover in SumaryForm

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -24,10 +24,35 @@ test("Checkbox enables button on first click and disables on second click", () =
   });
 
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
-
   userEvent.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
   userEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", () => {
+  render(<SummaryForm />);
+  //popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
+  //popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+  //popover disappears when we mouse out
+
+  userEvent.unhover(termsAndConditions);
+
+  const nullPopoverAgain = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopoverAgain).not.toBeInTheDocument();
 });
