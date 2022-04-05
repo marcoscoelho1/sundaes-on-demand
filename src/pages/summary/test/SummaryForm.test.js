90dad44f542120c85ae2b83355737a27c32f1e83
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2022-04-05 20:12:59 -0300

    feat: test assynchronous function when popver disappers from screen

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -31,7 +35,7 @@ test("Checkbox enables button on first click and disables on second click", () =
   expect(confirmButton).toBeDisabled();
 });
 
-test("popover responds to hover", () => {
+test("popover responds to hover", async () => {
   render(<SummaryForm />);
   //popover starts out hidden
   const nullPopover = screen.queryByText(
@@ -50,9 +54,7 @@ test("popover responds to hover", () => {
 
   userEvent.unhover(termsAndConditions);
 
-  const nullPopoverAgain = screen.queryByText(
-    /no ice cream will actually be delivered/i
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
   );
-
-  expect(nullPopoverAgain).not.toBeInTheDocument();
 });
