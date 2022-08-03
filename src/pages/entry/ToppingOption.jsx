344b0f43b92ff295d64c2e2d2209fcc3be0554d7
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2022-08-02 21:51:22 -0300

    feat: starts to implement grand-total tests

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,12 +1,22 @@
+import { Form } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 
-const ToppingOption = ({ name, imagePath }) => (
+const ToppingOption = ({ name, imagePath, updateItemCount }) => (
   <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
     <img
       style={{ width: "75%" }}
       src={`http://localhost:3030/${imagePath}`}
       alt={`${name} topping`}
     />
+    <Form.Group controlId={`${name}-topping-checkbox`}>
+      <Form.Check
+        type="checkbox"
+        onChange={(e) => {
+          updateItemCount(name, e.target.checked ? 1 : 0);
+        }}
+        label={name}
+      />
+    </Form.Group>
   </Col>
 );
 
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import OrderEntry from "../OrderEntry";
 
 test("update scoop subtotal when scoops change", async () => {
   render(<Options optionType="scoops" />);
@@ -26,4 +27,41 @@ test("update scoop subtotal when scoops change", async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when toppings change", async () => {
+  // render parent component
+  render(<Options optionType="toppings" />);
+
+  // make sure total starts out at $0.00
+  const toppingsTotal = screen.getByText("Toppings total $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  //add cherries and check subtotal
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  //add hot fudge and check subtotal
+  const hotFudgeCheckbox = screen.getByRole("checkbox", { name: "Hot fudge" });
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("3.00");
+
+  //remove hot fudge and check subtotal
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+});
+
+describe("grand total", () => {
+  test("grand total starts at $0.00", () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByText("Grand total: $", { exact: false });
+    expect(grandTotal).toHaveTextContent("0.00");
+  });
+  test("grand total updates properly if scoop is added first", () => {});
+  test("grand total updates properly if topping is added first", () => {});
+  test("grand total updates properly if item is removed", () => {});
 });
